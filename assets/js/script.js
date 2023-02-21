713039867a63da31c2be38e94b2dbdbd95878dd4
Alexandre <102592431+Foxenz@users.noreply.github.com>
2023-02-21 23:17:55 +0100

    Login via Ajax & popup error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,43 @@
 $(document).ready(function() {
     // Cache le bouton de connexion et d'inscription au chargement de la page et le réaffiche si les champs sont remplis
-    $('button[type="submit"]').hide();
+    $('#connectButton').hide();
     $('form input').on('keyup', function() {
         var email = $('input[name="email"]').val();
         var password = $('input[name="password"]').val();
         
         if (email != '' && password != '') {
-            $('button[type="submit"]').show();
+            $('#connectButton').show();
         } else {
-            $('button[type="submit"]').hide();
+            $('#connectButton').hide();
         }
+    });
+
+    // Système de connexion
+    $("#loginForm").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            url:  "../../php/login.php",
+            data: {
+                email : $("#email").val(),
+                password : $("#password").val()
+                }, 
+            success: function(data){
+                if(data == "Success"){
+                    window.location.href = "../../pages/landing/landing.php";
+                }else if(data == "Erreur"){
+                    $("#error").css("color", "red").html("Mot de passe ou email incorrect");
+                    $("#error").css("display", "block");
+                }
+            },
+            error: function(data){
+                alert("Erreur");
+            }
+        });
+    });
+
+    // Si on clique sur le message d'erreur, on le cache
+    $("#error").click(function(){
+        $("#error").css("display", "none");
     });
 });
--- index.php
+++ index.php
@@ -20,50 +20,19 @@
 
 <body>
     <div class="login-form">
-        <?php
-        if (isset($_GET['login_err'])) {
-            $err = htmlspecialchars($_GET['login_err']);
-
-            switch ($err) {
-                case 'password':
-        ?>
-                    <div class="alert alert-danger">
-                        <strong>Erreur</strong> mot de passe incorrect
-                    </div>
-                <?php
-                    break;
-
-                case 'email':
-                ?>
-                    <div class="alert alert-danger">
-                        <strong>Erreur</strong> email incorrect
-                    </div>
-                <?php
-                    break;
-
-                case 'already':
-                ?>
-                    <div class="alert alert-danger">
-                        <strong>Erreur</strong> compte non existant
-                    </div>
-        <?php
-                    break;
-            }
-        }
-        ?>
-
-        <form action="../php/login.php" method="post">
+        <form id="loginForm">
             <h2 class="text-center">Connexion</h2>
             <div class="form-group">
-                <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
+                <input type="email" name="email" id="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
             </div>
             <div class="form-group">
-                <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
+                <input type="password" name="password" id="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
             </div>
             <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block">Connexion</button>
+                <button id="connectButton" type="submit" class="btn btn-primary btn-block">Connexion</button>
             </div>
         </form>
+        <div class="alert alert-danger" id="error"></div>
         <p class="text-center"><a href="./pages/register/register.php">Inscription</a></p>
     </div>
 </body>
